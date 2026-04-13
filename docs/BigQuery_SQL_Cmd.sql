60f451f2de154451ea650bf0d17199e37b53b1f0
AM <ammartin88@protonmail.com>
2026-04-13 18:33:22 -0400

    Update docker-compose with env vars, add instruction sfor BigQuery file, removed main.py

--- docs/BigQuery_SQL_Cmd.sql
+++ docs/BigQuery_SQL_Cmd.sql
@@ -1,8 +1,8 @@
 -- Creating external tables from source parquet files
-CREATE OR REPLACE EXTERNAL TABLE `my-project-name.hard_drive_dataset.hard_drive_data_tbl_ext`
+CREATE OR REPLACE EXTERNAL TABLE `my-project-name.hard_drive_dataset.hard_drive_data_tbl_ext` --UPDATE PROJECT NAME
 OPTIONS (
   format = 'PARQUET',
-  uris = ['gs://my-bucket-name/raw_data/*.parquet']
+  uris = ['gs://my-bucket-name/raw_data/*.parquet'] --UPDATE BUCKET NAME
 );
 
 -- Validate record counts
@@ -19,7 +19,7 @@ ORDER BY yr
 ;
 
 -- Create materialized table
-CREATE OR REPLACE TABLE `my-project-name.hard_drive_dataset.hard_drive_data_tbl`
+CREATE OR REPLACE TABLE `my-project-name.hard_drive_dataset.hard_drive_data_tbl` --UPDATE PROJECT NAME
 PARTITION BY year_month AS
 SELECT
 date
@@ -28,5 +28,5 @@ date
 , serial_number, model, capacity_bytes, failure, datacenter, cluster_id, vault_id, pod_id, pod_slot_num
 , is_legacy_format, smart_5_normalized, smart_5_raw, smart_187_normalized, smart_187_raw, smart_188_normalized
 , smart_188_raw, smart_197_normalized, smart_197_raw, smart_198_normalized, smart_198_raw 
-FROM my-project-name.hard_drive_dataset.hard_drive_data_tbl_ext
+FROM my-project-name.hard_drive_dataset.hard_drive_data_tbl_ext --UPDATE PROJECT NAME
 ;
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from hard-drive-failure-analytics-dashboard!")
-
-
-if __name__ == "__main__":
-    main()
